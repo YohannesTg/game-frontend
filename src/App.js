ba1d3756eaa222a7334583164af3fa5bccce425f
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-06-02 13:03:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import Header from './Header'
 
 
 function App(props) { 
- 
   
-  const [trailNum, setTrialNum]=React.useState([1])
-  const actualNum=props.target
-  
+  const [trailNum, setTrialNum]=React.useState([1])  
 
   function NewGuess(){
 setTrialNum((prevValue)=>[...prevValue, prevValue[prevValue.length-1]+1])
@@ -19,7 +16,7 @@ setTrialNum((prevValue)=>[...prevValue, prevValue[prevValue.length-1]+1])
 
   return(<div className="container-fluid text-center  ">
     <Header trialNum={trailNum.length}/>
- {trailNum.map((value)=><Guess key={value} name={value}  actualNum={actualNum} NewGuess={NewGuess}/>)}
+ {trailNum.map((value)=><Guess key={value} name={value} NewGuess={NewGuess}/>)}
 
  
     </div>
