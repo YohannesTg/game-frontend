d62fdf23bfd2fef83cef89b3fedd35451ad9c9ab
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-06-18 23:28:04 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-export default function Guess(props){
-    return(
-      <div className="container pt-4">
-      <div className='row rounded-circle bg-secondary d-flex align-items-center justify-content-center text-white' style={{width: "40px", height: "40px"}}>{props.trialNum}</div>
-      <div className="row justify-content-center">
-        
-        <div className="col-2 col-md-1 btn btn-primary me-2" >
-  N
+export default function Guess(props) {
+    return (
+        <div className="container pt-4">
+            <div className="row justify-content-start">
+                <div className="col-auto d-flex flex-column align-items-center">
+                    <div className='rounded-circle bg-secondary d-flex align-items-center justify-content-center text-white mb-2' style={{ width: "40px", height: "40px" }}>
+                        {props.trialNum}
+                    </div>
+                    <h5>{props.userName}</h5>
+                </div>
+            </div>
+            <div className="row justify-content-center mt-3">
+                <div className="col-2 col-md-1 btn btn-primary me-2">
+                    N
+                </div>
+                <div className="col-2 col-md-1 btn btn-primary">
+                    O
+                </div>
+            </div>
         </div>
-        <div className="col-2 col-md-1 btn btn-primary" >
-  O
-        </div>
-      </div>
-      </div>
-      
-      
-    )
+    );
 }
