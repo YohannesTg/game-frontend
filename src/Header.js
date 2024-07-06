3d0b4cd1be817350330859423b05943512c66d97
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-07-06 19:14:34 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export default function Guess(props) {
        <div className="container pt-4">
   <div className="row justify-content-between">
     {/* Opponent 1 Section */}
-    <div className="col-4 text-start">
+    <div className="col-2 text-start">
       <div className="container">
         <div className="row text-white bg-secondary" style={{ height: "30px" }}>
           <div className="col d-flex align-items-center">
@@ -28,7 +28,7 @@ export default function Guess(props) {
     </div>
 
     {/* Opponent 2 Section */}
-    <div className="col-4 text-end">
+    <div className="col-2 text-end">
       <div className="container">
         <div className="row text-white bg-secondary" style={{ height: "30px" }}>
           <div className="col d-flex align-items-center justify-content-end">
