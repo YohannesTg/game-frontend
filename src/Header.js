dc23e6b6f644e77d2b55922581dd04e364c46348
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-07-06 19:12:28 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,51 +2,58 @@ import React from 'react';
 
 export default function Guess(props) {
     return (
-        <div className="container pt-4">
-<div className="row justify-content-between ">
-  {/* Opponent 1 Section */}
-  <div className="col-4 text-start">
-        <div className="container">
-    <div className="row text-white bg-secondary" style={{ height: "6 px" }}><div className="col"><span>{props.userName}</span></div></div>
-      <div className="row btn btn-warning">
-    <div className="col"><span>Score:</span>
-        3</div>
-        
-      </div>
-      <div className="row btn btn-warning">
-            <div className="col">        <span>Trial:</span>
-        {props.trialNum}</div>
-
-    </div>
+       <div className="container pt-4">
+  <div className="row justify-content-between">
+    {/* Opponent 1 Section */}
+    <div className="col-4 text-start">
+      <div className="container">
+        <div className="row text-white bg-secondary" style={{ height: "30px" }}>
+          <div className="col d-flex align-items-center">
+            <span>{props.userName}</span>
+          </div>
         </div>
-    
-  </div>
-
-  {/* Opponent 2 Section */}
-  <div className="col-4 text-end">
-    <div className="container">
-    <div className="row text-white bg-secondary" style={{ height: "6 px" }}><div className="col"><span>{props.oppName}</span></div></div>
-      <div className="row btn btn-warning">
-      <div className="col"><span>Score:</span>
-        3</div>
-
+        <div className="row btn btn-warning my-2">
+          <div className="col d-flex align-items-center">
+            <span>Score:</span>
+            3
+          </div>
+        </div>
+        <div className="row btn btn-warning my-2">
+          <div className="col d-flex align-items-center">
+            <span>Trial:</span>
+            {props.trialNum}
+          </div>
+        </div>
       </div>
-      <div className="row btn btn-warning">
-            <div className="col"><span>Trial:</span>
-        {props.trialNum}</div>
-        
     </div>
+
+    {/* Opponent 2 Section */}
+    <div className="col-4 text-end">
+      <div className="container">
+        <div className="row text-white bg-secondary" style={{ height: "30px" }}>
+          <div className="col d-flex align-items-center justify-content-end">
+            <span>{props.oppName}</span>
+          </div>
+        </div>
+        <div className="row btn btn-warning my-2">
+          <div className="col d-flex align-items-center justify-content-end">
+            <span>Score:</span>
+            5
+          </div>
+        </div>
+        <div className="row btn btn-warning my-2">
+          <div className="col d-flex align-items-center justify-content-end">
+            <span>Trial:</span>
+            {props.trialNum}
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  <div className="row justify-content-center mt-3">
+    <div className="col-2 col-md-1 btn btn-primary me-2">N</div>
+    <div className="col-2 col-md-1 btn btn-primary">O</div>
   </div>
 </div>
-            <div className="row justify-content-center mt-3">
-                <div className="col-2 col-md-1 btn btn-primary me-2">
-                    N
-                </div>
-                <div className="col-2 col-md-1 btn btn-primary">
-                    O
-                </div>
-            </div>
-        </div>
     );
 }
