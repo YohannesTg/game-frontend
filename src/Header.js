4b82e3b06b91571bee02e0bceede4b1a4da4e805
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-07-07 22:00:49 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,21 +5,21 @@ export default function Guess(props) {
        <div className="container pt-4">
   <div className="row justify-content-between">
     {/* Opponent 1 Section */}
-    <div className="col-2 col-md-5  text-start">
+    <div className="col-4 col-md-5  text-start">
       <div className="container">
-        <div className="row text-white bg-secondary" style={{ height: "30px" }}>
-          <div className="col d-flex align-items-center rounded-5 justify-content-center">
+        <div className="row text-white bg-secondary rounded-5" style={{ height: "30px" }}>
+          <div className="col d-flex align-items-center justify-content-center">
             <span>{props.userName}</span>
           </div>
         </div>
-        <div className="row bg-warning my-1">
-          <div className="col d-flex align-items-center rounded-5  justify-content-center">
+        <div className="row bg-warning my-1 rounded-5">
+          <div className="col d-flex align-items-center justify-content-center">
             <span>Score:</span>
             3
           </div>
         </div>
-        <div className="row bg-warning my-1">
-          <div className="col d-flex align-items-center rounded-5  justify-content-center">
+        <div className="row bg-warning my-1 rounded-5">
+          <div className="col d-flex align-items-center justify-content-center">
             <span>Trial:</span>
             {props.trialNum}
           </div>
@@ -28,21 +28,21 @@ export default function Guess(props) {
     </div>
 
     {/* Opponent 2 Section */}
-    <div className="col-2 col-md-5 text-end">
+    <div className="col-4 col-md-5 text-end">
       <div className="container">
-        <div className="row text-white bg-secondary" style={{ height: "30px" }}>
-          <div className="col d-flex align-items-center rounded-5 justify-content-end">
+        <div className="row text-white bg-secondary rounded-5" style={{ height: "30px" }}>
+          <div className="col d-flex align-items-center  justify-content-end">
             <span>{props.oppName}</span>
           </div>
         </div>
-        <div className="row bg-warning my-1">
-          <div className="col d-flex align-items-center rounded-5  justify-content-center">
+        <div className="row bg-warning my-1 rounded-5">
+          <div className="col d-flex align-items-center justify-content-center">
             <span>Score:</span>
             5
           </div>
         </div>
-        <div className="row bg-warning my-1">
-          <div className="col d-flex align-items-center rounded-5 justify-content-center">
+        <div className="row bg-warning my-1 rounded-5">
+          <div className="col d-flex align-items-center justify-content-center">
             <span>Trial:</span>
             {props.trialNum}
           </div>
