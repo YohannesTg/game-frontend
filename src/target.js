8d207638241c3f1a9745c0a4a50bc39c9ea02f3f
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-12-08 23:20:51 +0300

    Update target.js

--- src/target.js
+++ src/target.js
@@ -65,7 +65,7 @@ export default function Target() {
             </div>
             {/* Input + Button Responsive Row */}
             <div className="row justify-content-center align-items-center">
-              <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+              <div className="col-12 col-sm-4 col-md-6 col-lg-8">
                 <div className="input-group input-group-lg mb-3" style={{ maxWidth: '100%' }}>
                   <input
                     type="text"
