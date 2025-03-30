7c351ea6c8597f0bbf73c27d566c4c80d92999ce
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2025-03-30 18:16:31 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,14 @@ export default function Header({ userName, oppName, score1, score2, trialNum, tr
     <div className="header-container">
       <div className="container">
         <div className="player-info">
-          {/* Player 1 */}
-          <div className="player-1">
+          <div>
             <div className="player-name">{userName}</div>
             <div className="score-label">
               Score: {score1} | Trials: {trialNum}
             </div>
           </div>
-
-          {/* Player 2 */}
-          <div className="player-2">
+          
+          <div className="text-end">
             <div className="opponent-name">{oppName || 'Opponent'}</div>
             <div className="score-label">
               Score: {score2} | Trials: {trial2}
