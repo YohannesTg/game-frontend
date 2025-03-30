24baeaab34a06722cf21b4cea027aecfdb922979
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2025-03-30 14:34:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Header from './Header';
 function App(props) {
   const [trailNum, setTrialNum] = React.useState([1]);
   const [opponentUsername, setOpponentUsername] = React.useState("");
-  const [trial2, setTrial2] = React.useState([1]);
-  const [score1, setScore1] = React.useState([0]);
-  const [score2, setScore2] = React.useState([0]);
+  const [trial2, setTrial2] = React.useState(1);
+  const [score1, setScore1] = React.useState(0);
+  const [score2, setScore2] = React.useState(0);
+  const [numberState, setNumberState] = React.useState(0);
+  const [orderState, setOrderState] = React.useState(0);
 
-  function gameScore(trialB, scoreA, scoreB) {
+  function gameScore(trialB, scoreA, scoreB, number, order) {
     setTrial2(trialB);
     setScore1(scoreA);
     setScore2(scoreB);
+    setNumberState(number);
+    setOrderState(order);
   }
 
   function oppName(name) {
@@ -24,7 +28,8 @@ function App(props) {
     setTrialNum((prevValue) => [...prevValue, prevValue[prevValue.length - 1] + 1]);
   }
 
-  return (    <div className="container-fluid">
+  return (
+   <div className="container-fluid">
       <Header
         trialNum={trailNum.length}
         userName={props.userName}
@@ -32,24 +37,24 @@ function App(props) {
         trial2={trial2}
         score1={score1}
         score2={score2}
+        numberState={numberState}
+        orderState={orderState}
       />
 
-      <div
-        style={{
-          marginTop: '180px',
-          maxHeight: 'calc(100vh - 180px)',
-          overflowY: 'auto',
-          padding: '2rem',
-          scrollBehavior: 'smooth'
-        }}
-      >
+      <div style={{
+        marginTop: '220px',
+        maxHeight: 'calc(100vh - 220px)',
+        overflowY: 'auto',
+        padding: '2rem',
+        scrollBehavior: 'smooth'
+      }}>
         <div className="container">
           {trailNum.map((value) => (
             <Guess
               key={value}
               chatId={props.chatId}
               userId={props.userId}
-              oppName={oppName}
+              oppName={setOpponentUsername}
               name={value}
               gameScore={gameScore}
               NewGuess={NewGuess}
